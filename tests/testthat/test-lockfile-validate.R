2dd6a114f6acb3ecd8d00bda310feb7dddab5436
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-04-30 15:27:32 -0400

    Initial testing

--- tests/testthat/test-lockfile-validate.R
+++ tests/testthat/test-lockfile-validate.R
@@ -55,6 +55,62 @@ test_that("a known-good lockfile passes validation", {
 })
 
 
+test_that("a known-good lockfile with extra fields passes validation", {
+  # Lockfile adds a R$Nickname field not present in the schema
+  lockfile <- '
+{
+  "R": {
+    "Version": "4.2.3",
+    "Nickname": "Shortstop Beagle",
+    "Repositories": [
+      {
+        "Name": "CRAN",
+        "URL": "https://cloud.r-project.org"
+      },
+      {
+        "Name": "BioCsoft",
+        "URL": "https://bioconductor.org/packages/3.8/bioc"
+      }
+    ]
+  },
+  "Python": {
+    "Version": "3.10.12",
+    "Type": "virtualenv",
+    "Name": "./renv/python/virtualenvs/renv-python-3.10"
+  },
+  "Bioconductor": {
+    "Version": "3.8"
+  },
+  "Packages": {
+    "markdown": {
+      "Package": "markdown",
+      "Version": "1.0",
+      "Source": "Repository",
+      "Repository": "CRAN",
+      "Hash": "4584a57f565dd7987d59dda3a02cfb41"
+    },
+    "mime": {
+      "Package": "mime",
+      "Version": "0.12.1",
+      "Source": "GitHub",
+      "RemoteType": "github",
+      "RemoteHost": "api.github.com",
+      "RemoteUsername": "yihui",
+      "RemoteRepo": "mime",
+      "RemoteRef": "main",
+      "RemoteSha": "1763e0dcb72fb58d97bab97bb834fc71f1e012bc",
+      "Requirements": [
+        "tools"
+      ],
+      "Hash": "c2772b6269924dad6784aaa1d99dbb86"
+    }
+  }
+}
+'
+  expect_no_error(lockfile_validate(lockfile = lockfile))
+  expect_true(lockfile_validate(lockfile = lockfile))
+})
+
 test_that("a custom schema file can be used for successful validation", {
   # Custom schema adds a required R$Nickname field present in the lockfile
   lockfile <- '
